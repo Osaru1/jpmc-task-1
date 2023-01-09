51c1ee944b3b9f2aa573aed5c198908e93eb610b
Osaru Osagie <osarumeno1@gmail.com>
2023-01-08 21:39:28 -0500

    task1

--- client.py
+++ client.py
@@ -58,5 +58,4 @@ if __name__ == "__main__":
             correct_price[stock]= price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
         print("Ratio %s" % getRatio(correct_price['ABC'], correct_price['DEF']))
